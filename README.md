35e704a7a7680a555e68397fa51825a55d62df43
Alistair Willis <alistairrwillis@gmail.com>
2017-03-25 23:17:29 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Writing Functional Javascript with Ramda
+
+https://app.pluralsight.com/library/courses/javascript-ramda-functional/discussion 
+https://www.themoviedb.org/account/arwl/api 
---
+++ app/src/apikey.js
@@ -0,0 +1,6 @@
+const apiKey = '6cfc010364ebb513a59f90d51ca940c5';
+
+module.exports = apiKey;
+
+// https://api.themoviedb.org/3/movie/550?api_key=6cfc010364ebb513a59f90d51ca940c5
+// eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Y2ZjMDEwMzY0ZWJiNTEzYTU5ZjkwZDUxY2E5NDBjNSIsInN1YiI6IjU4ZDY3NjAxOTI1MTQxMWY4OTA0MDYwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8_6XT_ElE4JPfAHKlic9asLGGv3Wav8pcXJ_9XMN96s
